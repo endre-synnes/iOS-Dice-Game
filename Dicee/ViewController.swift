748af3fa8b690db457cbc57c4a27827ecc0da8da
synend16 <synend16@student.westerdals.no>
2018-09-19 22:25:44 +0200

    completed dice game

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImgView1: UIImageView!
     @IBOutlet weak var diceImgView2: UIImageView!
@@ -17,9 +22,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        rollDices()
+    }
+    
+    func rollDices() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImgView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        
+        diceImgView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        rollDices()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollDices()
     }
 
 }
